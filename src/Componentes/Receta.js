d14e9b593cfb2b12496941aaa023c0d2b02ca172
VickyDuarte1 <victoria.durte@gmail.com>
2022-12-22 20:04:48 -0300

    probandorecetario

---
+++ src/Componentes/Receta.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Receta(props){
+    return (
+
+<div className='contenedorpcpal'>
+
+<p className='nombre-receta'> {props.nombre}</p>
+
+<div className='ingredientes-receta'>
+<img className='img1'
+src={require (`../Imagenes/${props.imagen}`) }
+alt= 'fotoreceta' />
+
+<ul className= 'Ingredientes'>
+    <p>Ingredientes:</p>
+    <p>{props.ingredientes}</p>
+    </ul>
+    
+</div>
+<p className= 'pasos-receta'> {props.pasos}</p>
+<br></br>
+
+</div>
+    );
+};
+
+export default Receta;
